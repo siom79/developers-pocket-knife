d51e01a39dfcbd06552cfa95eeb2fc7585df70f6
siom79 <martin.mois@googlemail.com>
2014-03-05 21:19:10 +0100

    improved format of output

--- src/main/java/developers/pocket/knife/ui/tools/regex/RegExModel.java
+++ src/main/java/developers/pocket/knife/ui/tools/regex/RegExModel.java
@@ -14,29 +14,23 @@ public class RegExModel {
         StringBuilder sb = new StringBuilder();
         String lines[] = testData.split("\\r?\\n");
         for (String line : lines) {
+            sb.append(line + "\n");
             Pattern pattern = Pattern.compile(regEx);
             Matcher matcher = pattern.matcher(line);
             int count = 0;
             while (matcher.find()) {
                 count++;
-                if (count > 1) {
-                    sb.append(" ");
-                }
-                sb.append(count + ". match:");
-                sb.append(line.substring(matcher.start(), matcher.end()));
+                sb.append("\t" + count + ". match:");
+                sb.append(line.substring(matcher.start(), matcher.end()) + "\n");
                 int groupCount = matcher.groupCount();
                 for (int i = 1; i <= groupCount; i++) {
                     String group = matcher.group(i);
-                    if(i == 1 && count >= 1) {
-                        sb.append(" ");
-                    }
-                    if (i > 1) {
-                        sb.append(" ");
-                    }
-                    sb.append(i + ". group:" + group);
+                    sb.append("\t\t" + i + ". group:" + group+"\n");
                 }
             }
-            sb.append("\n");
+            if(count == 0) {
+                sb.append("\tNo match.\n");
+            }
         }
         return sb.toString();
     }
